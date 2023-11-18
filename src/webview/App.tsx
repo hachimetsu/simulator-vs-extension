8b6b4f78bcbbf226400f721502f684dcacd6a974
Hachi Metsu <hachimetsu@gmail.com>
2023-11-18 17:02:32 +0000

    all set

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -3,8 +3,8 @@ export default function App(){
   const [count, setCount] = React.useState(0);
   return <>
     <div className="terminal w-screen h-screen flex justify-center items-center">
-      <span className="count">count: {count}</span>
-      <button type="button" onClick={()=>setCount((prev)=>prev+1)}>count</button>
+      <span className="count">count: {count}</span> <br/>
+      <button type="button" className="button " onClick={()=>setCount((prev)=>prev+1)}>count</button>
     </div>
   </>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), renameOutfiles()],
   build: {
-    outDir: "out",
+    outDir: "media/webview",
+    assetsDir: "",
+    ssrManifest: false,
+    rollupOptions: {
+      input: "/src/webview/main.tsx"
+    }
   },
+  ssr: {
+    noExternal: true
+  }
 });
 
+function renameOutfiles(): PluginOption {
+    return {
+        name: "renameOutfiles",
+        generateBundle: (options, bundle, isWrite)=>{
+            for (const [fileName, chunk] of Object.entries(bundle)) {
+                // Customize the new filename based on the file type
+                let newFileName: string;
 
+                if (fileName.endsWith('.js')) {
+                    newFileName = 'main.js';
+                } else if (fileName.endsWith('.css')) {
+                    newFileName = 'style.css';
+                } else {
+                    // If it's not a JS or CSS file, keep the original filename
+                    newFileName = fileName;
+                }
+                // Rename the file
+                chunk.fileName = newFileName;
+            }
+        }
+    };
+}
